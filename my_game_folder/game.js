d7d530d5de87ad9a6dcb5f7852d3ae99401f5b73
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-10-20 20:14:03 -0400

    Syntax

--- my_game_folder/game.js
+++ my_game_folder/game.js
@@ -1,17 +1,21 @@
-window.onload = function() {
-    var config = {
-        width: 256,
-        height: 272,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2],
-        pixelArt: true,
-        physics: {
-            default: "arcade",
-            arcade: {
-                debug: false
-            }
+var gameSettings = {
+    playerSpeed: 200,
+}
+
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
         }
     }
+}
 
+window.onload = function() {
     var game = new Phaser.Game(config);
 }
